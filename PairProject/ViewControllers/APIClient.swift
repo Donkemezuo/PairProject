1eb2eb48f1dd384f625e7020627de39a5f51db62
ibraheemraw <ibraheemrawlinson@pursuit.org>
2019-01-10 19:42:43 -0500

    Added icons to the tab bars and fixed the reload of the magic cards vc. project completion

--- PairProject/ViewControllers/APIClient.swift
+++ PairProject/ViewControllers/APIClient.swift
@@ -15,12 +15,14 @@ static func getMagicCard(completionHandler:@escaping(AppError?,[MagicCardDetails
     let magicCardUrlString = "https://api.magicthegathering.io/v1/cards?contains=imageUrl"
     NetworkHelper.shared.performDataTask(endpointURLString: magicCardUrlString) { (error, data, response) in
         if let error = error {
+
             completionHandler(error,nil)
         } else if let data = data {
             do {
         let magicCards = try JSONDecoder().decode(MagicCards.self,from: data)
                 let details = magicCards.cards
-                completionHandler(nil,details)
+                let filteredDetails = details.filter{$0.imageUrl != nil}
+                completionHandler(nil, filteredDetails)
             } catch {
                 completionHandler(AppError.decodingError(error),nil)
             }
